5cbb42057ade20c71a7c6f294dbfd2d6d20d9b44
shreeparab1890 <shrikrishnaparab18@gmail.com>
2022-07-07 20:07:15 +0530

    final commit

---
+++ src/components/database.js
@@ -0,0 +1,44 @@
+export const Leaderboard = [
+  {
+    name: "Raj",
+    location: "India",
+    score: 1550,
+    img: "https://img.freepik.com/premium-vector/upset-female-user-profile-avatar-is-woman-character-screen-saver-with-emotions_505620-628.jpg?w=740",
+    dt: "2022-07-04",
+  },
+  {
+    name: "Robert",
+    location: "USA",
+    score: 2310,
+    img: "https://img.freepik.com/premium-vector/female-user-profile-avatar-is-woman-character-screen-saver-with-emotions_505620-617.jpg?w=740",
+    dt: "2022-06-15",
+  },
+  {
+    name: "John",
+    location: "Chaina",
+    score: 350,
+    img: "https://img.freepik.com/premium-vector/female-user-profile-avatar-is-woman-character-screen-saver-with-emotions_505620-556.jpg?w=740",
+    dt: "2022-06-25",
+  },
+  {
+    name: "Michael",
+    location: "Japan",
+    score: 2100,
+    img: "https://img.freepik.com/premium-vector/female-user-profile-avatar-is-woman-character-screen-saver-with-emotions_505620-542.jpg?w=740",
+    dt: "2021-10-23",
+  },
+  {
+    name: "Patricia",
+    location: "London",
+    score: 1250,
+    img: "https://img.freepik.com/premium-vector/female-user-profile-avatar-is-woman-character-screen-saver-with-emotions_505620-562.jpg?w=740",
+    dt: "2022-01-22",
+  },
+  {
+    name: "Jennifer",
+    location: "Canada",
+    score: 5250,
+    img: "https://img.freepik.com/premium-vector/female-user-profile-avatar-is-woman-character-screen-saver-with-emotions_505620-608.jpg?w=740",
+    dt: "2022-01-21",
+  },
+];
